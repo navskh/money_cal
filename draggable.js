b623f05901c2f73072778e1972a21bd8d478beef
Young <navskh@naver.com>
2023-09-30 01:34:39 +0900

    update 0930

--- draggable.js
+++ draggable.js
@@ -1,20 +1,19 @@
 new Sortable(sortableList, {
   group: 'shared',
   animation: 150,
-  swapThreshold: 0.0,
+  swapThreshold: 0.2,
   forceFallback: true,
   scroll: false,
+  delay: 200, // 클릭 후 드래그 시작까지의 시간 (200ms)
+  touchStartThreshold: 10, // 터치 디바이스에서 드래그 시작 최소 거리 (10px)
   onEnd(evt) {
     const draggedEl = evt.item;
-
     let closestItem = null;
     let closestDistance = Infinity;
-
-    // 모든 항목을 순회하면서 마우스 위치와의 거리를 계산
+    // // 모든 항목을 순회하면서 마우스 위치와의 거리를 계산
     sortableList.querySelectorAll('li').forEach(item => {
       const rect = item.getBoundingClientRect();
       let clientY;
-
       if (evt.originalEvent.touches && evt.originalEvent.touches.length) {
         clientY = evt.originalEvent.touches[0].clientY;
       } else if (
@@ -25,21 +24,49 @@ new Sortable(sortableList, {
       } else {
         clientY = evt.originalEvent.clientY;
       }
-
       const distance = Math.abs(rect.top + rect.height / 2 - clientY);
-
       if (distance < closestDistance) {
         closestDistance = distance;
         closestItem = item;
       }
     });
-
-    console.log(closestItem);
-
     if (closestItem && closestItem !== draggedEl) {
-      closestItem.innerHTML =
-        draggedEl.textContent + '<br> ' + closestItem.innerHTML;
-      draggedEl.remove();
+      mergeList(closestItem, draggedEl);
     }
   },
 });
+
+function mergeList(target, dragged) {
+  const targetSeq = target.id.split('_')[1];
+  const draggedSeq = dragged.id.split('_')[1];
+  const targetBroName = target.querySelector(`#broName_${targetSeq}`).innerText;
+  const draggedBroName = dragged.querySelector(
+    `#broName_${draggedSeq}`,
+  ).innerText;
+
+  const dragObject = thisBroList.find(bro => bro.name === draggedBroName);
+  const draggedMenu = dragObject.menu;
+  const draggedPrice = dragObject.price;
+  console.log(draggedMenu, draggedPrice);
+
+  if (targetBroName === draggedBroName) return;
+  else {
+    thisBroList.forEach((bro, index) => {
+      if (bro.name === targetBroName) {
+        bro.name += `, ${draggedBroName}`;
+        if (draggedMenu !== '') {
+          bro.menu.push(...draggedMenu);
+          bro.price.push(...draggedPrice);
+        }
+      }
+    });
+    thisBroList.splice(
+      thisBroList.findIndex(bro => bro.name === draggedBroName),
+      1,
+    );
+
+    makeList();
+
+    console.log(thisBroList);
+  }
+}
--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+class Brother {
+  constructor(name, menu, price) {
+    this.name = name;
+    this.menu = menu;
+    this.price = price;
+  }
+}
+
+function createBrother(name, menu, price) {
+  return new Brother(name, menu, price);
+}
+
 const broList = [
   '임세범',
   '강민',
@@ -15,33 +27,56 @@ const broList = [
   '장준형',
 ];
 
+thisBroList = [];
+
+function makeBroObject() {
+  let returnObjectList = [];
+  broList.forEach((bro, index) => {
+    returnObjectList.push(createBrother(bro, [''], [0]));
+  });
+
+  return returnObjectList;
+}
+
 const data = menu;
 
-function autocomplete(man, seq) {
-  const autoComplete = `
-  <li>
-    ${man}
+function ListComponent(bro, seq) {
+  const ListComponent = `
+  <li id='list_${seq}'>
+    <span id='broName_${seq}'>
+    ${bro.name}
+    </span>
     <div class="autocomplete-container" id="container_${seq}">
-      <input type="text" class="autocomplete-input" id="input_${seq}" oninput="inputComplete(${seq}, this)" placeholder="메뉴를 선택하세요">
-      <div id="result_${seq}" class="autocomplete-results"></div>
-      <button id="addBtn_${seq}" class="add-button" onclick="addRow(${seq})">+</button>
-    </div>
-  </li>
+      ${AutoComplete(bro, seq)}
+    </li>
+  </div>
   `;
 
-  return autoComplete;
+  return ListComponent;
 }
 
-function addRow(seq) {
-  $(`#addBtn_${seq}`).before(`
-        <input type="text" class="autocomplete-input" id="input_${seq}_${seq}" oninput="inputComplete('${seq}_${seq}', this)" placeholder="메뉴를 선택하세요">
-        <div id="result_${seq}_${seq}" class="autocomplete-results"></div>
-  `);
+function AutoComplete(bro, seq) {
+  let autocomplete = '';
+  bro.menu.forEach((menu, index) => {
+    autocomplete += `
+      <input 
+        type="text" 
+        class="autocomplete-input" 
+        id="input_${seq}_${index}" 
+        oninput="inputComplete('${seq}_${index}', this)" 
+        value="${menu ? menu : ''}"
+        placeholder="메뉴를 선택하세요"
+      />
+      <div id="result_${seq}_${index}" class="autocomplete-results"></div>
+    `;
+  });
+  return autocomplete;
 }
 
-function inputComplete(seq, element) {
+function inputComplete(idx, element) {
   const value = $(element).val();
-  $(`#result_${seq}`).empty();
+  const seq = idx.split('_')[0];
+  $(`#result_${idx}`).empty();
 
   if (value) {
     const matchedData = $.grep(data, function (item) {
@@ -50,26 +85,50 @@ function inputComplete(seq, element) {
 
     $.each(matchedData, function (index, item) {
       const div = $('<div>').text(`${item.name}, ${item.price}`);
-      $(`#result_${seq}`).append(div);
+      div.on('click', function (e) {
+        const thisMenu = $(this).text().split(', ')[0];
+        const thisPrice = parseInt(
+          $(this).text().split(', ')[1].replace(',', '').replace('원', ''),
+        );
+        const thisBro = $(`#broName_${seq}`).text().trim();
+        updateBroList(thisBro, thisMenu, thisPrice, idx);
+
+        $(`#input_${idx}`).val(thisMenu);
+        $(`#result_${idx}`).empty();
+      });
+      $(`#result_${idx}`).append(div);
     });
 
-    $(`#result_${seq}`).show();
-    $(`#result_${seq}`).on('click', 'div', function () {
-      $(`#input_${seq}`).val($(this).text());
-      $(`#result_${seq}`).empty();
-    });
+    $(`#result_${idx}`).show();
   } else {
-    $(`#result_${seq}`).empty();
+    $(`#result_${idx}`).empty();
   }
 }
 
+function updateBroList(target, menu, price, idx) {
+  console.log(target, menu, price, idx);
+  const thisIndex = idx.split('_')[0];
+  const thisInputIndex = idx.split('_')[1];
+  thisBroList[thisIndex].menu[thisInputIndex] = menu;
+  thisBroList[thisIndex].price[thisInputIndex] = price;
+  console.log(thisBroList);
+}
+
 function makeList() {
-  broList.forEach((bro, index) => {
-    $('#sortableList').append(autocomplete(bro, index));
+  $('#sortableList').empty();
+  thisBroList.forEach((bro, index) => {
+    $('#sortableList').append(ListComponent(bro, index));
   });
 }
 
 $(document).ready(function () {
+  const initStore = JSON.parse(localStorage.getItem('broList'));
+  console.log(initStore);
+  if (initStore) {
+    thisBroList = initStore;
+  } else {
+    thisBroList = makeBroObject();
+  }
   makeList();
 
   $(document).on('click', function (event) {
